51e5dc79503600c559cc01c9ffb9d777509ec4bf
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-03-26 17:04:22 +0000

    Set displayMarker function

--- src/Map.js
+++ src/Map.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import services from './utils/services.js'
 
 const mapStyles = {
     width: '100vw',
     height: '100vh'
 };
 
+
+
 const MyMap = (props) => {
+
+  const displayMarker = () => {
+      return services.map((service, index) => {
+        return <Marker key={index} id={index} position={{
+         lat: services.lat,
+         lng: services.lng,
+       }}
+    />
+})
+}
     return (
         <Map
           google={props.google}
           zoom={6}
           style={mapStyles}
           initialCenter={{ lat: 53.507351, lng: 0.127758}}
-        />
+          >
+          {displayMarker()}
+        </Map>
     )
 }
+
 
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyDVVvArLl32Y8YQSPgSh2Mv89hOmdfIOQw'
